3452c1bb789affd01bf9e483046fb675d2565283
Rafael Dametto <cacotpds@gmail.com>
2023-08-01 19:57:44 -0300

    MyOwnHook2 - 2

--- MyOwnHook2/src/App.jsx
+++ MyOwnHook2/src/App.jsx
@@ -1,33 +1,114 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
+const isObjectEqual = (objA, objB) => {
+  return JSON.stringify(objA) === JSON.stringify(objB);
+};
+
+const useFetch = (url, options) => {
+  const [result, setResult] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [shouldLoad, setShouldLoad] = useState(false);
+  const urlRef = useRef(url);
+  const optionsRef = useRef(options);
+
+  useEffect(() => {
+    let changed = false;
+    if (!isObjectEqual(url, urlRef.current)) {
+      urlRef.current = url;
+      changed = true;
+    }
+    if (!isObjectEqual(options, optionsRef.current)) {
+      optionsRef.current = options;
+      changed = true;
+    }
+    if (changed) {
+      setShouldLoad((s) => !s);
+    }
+  }, [url, options]);
+
+  useEffect(() => {
+    let wait = false;
+    console.log('EFFECT', new Date().toLocaleDateString());
+    console.log(optionsRef.current.headers);
+    setLoading(true);
+
+    const fetchData = async () => {
+      await new Promise((r) => setTimeout(r, 1000));
+
+      try {
+        const response = await fetch(urlRef.current, optionsRef.current);
+        const jsonResult = await response.json();
+
+        if (!wait) {
+          setResult(jsonResult);
+          setLoading(false);
+        }
+      } catch (e) {
+        if (!wait) {
+          setLoading(false);
+        }
+        throw e;
+      }
+    };
+    fetchData();
+    return () => {
+      wait = true;
+    };
+  }, [shouldLoad]);
+
+  return [result, loading];
+};
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [postId, setPostId] = useState('');
+  const [result, loading] = useFetch(
+    'https://jsonplaceholder.typicode.com/posts/' + postId,
+    {
+      headers: {
+        abc: '1' + postId,
+      },
+    }
+  );
+
+  useEffect(() => {
+    console.log('ID do Post', postId);
+  }, [postId]);
+
+  if (loading) {
+    return (
+      <p style={{ color: 'red' }}>
+        {' '}
+        <strong>Loading...</strong>
+      </p>
+    );
+  }
+
+  const handleClick = (id) => {
+    setPostId(id);
+  };
+
+  if (!loading && result) {
+    return (
+      <div>
+        {result?.length > 0 ? (
+          result.map((p) => (
+            <div key={`post-${p.id}`} onClick={() => handleClick(p.id)}>
+              <p>{p.title}</p>
+            </div>
+          ))
+        ) : (
+          <div onClick={() => handleClick('')}>
+            <p>{result.title}</p>
+          </div>
+        )}
+      </div>
+    );
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Oi</h1>
     </>
   );
 }
