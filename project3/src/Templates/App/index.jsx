69012c262f72906c3febdc0e1f1851ed1cd683a8
Rafael Dametto <cacotpds@gmail.com>
2023-08-01 13:02:53 -0300

    Organização useEffect, useReducer, useContext 2...

--- project3/src/Templates/App/index.jsx
+++ project3/src/Templates/App/index.jsx
@@ -1,15 +1,18 @@
 // DOING THE ORGANIZATION WITH -> useEffect, useReducer, useContext and useRef
 import { Posts } from '../Components';
+import { CounterProvider } from '../Contexts/CounterProvider';
 import { PostsProvider } from '../Contexts/PostsProvider';
 import './App,.test';
 
 function App() {
   return (
-    <PostsProvider>
-      <div>
-        <Posts />
-      </div>
-    </PostsProvider>
+    <CounterProvider>
+      <PostsProvider>
+        <div>
+          <Posts />
+        </div>
+      </PostsProvider>
+    </CounterProvider>
   );
 }
 export default App;
--- project3/src/Templates/Components/index.jsx
+++ project3/src/Templates/Components/index.jsx
@@ -2,10 +2,18 @@ import { useContext, useEffect, useRef } from 'react';
 import { PostsContext } from '../Contexts/PostsProvider/Context';
 import { loadPosts } from '../Contexts/PostsProvider/Actions';
 
+import { CounterContext } from '../Contexts/CounterProvider/Context';
+import { incrementCounter } from '../Contexts/CounterProvider/Action';
+import { decrementCounter } from '../Contexts/CounterProvider/Action';
+
 export const Posts = () => {
   const isMounted = useRef(true);
+
   const postsContext = useContext(PostsContext);
   const { postsState, postsDispatch } = postsContext;
+
+  const counterContext = useContext(CounterContext);
+  const { counterState, counterDispatch } = counterContext;
 
   useEffect(() => {
     loadPosts(postsDispatch).then((dispatch) => {
@@ -20,6 +28,12 @@ export const Posts = () => {
 
   return (
     <div>
+      <button onClick={() => incrementCounter(counterDispatch)}>
+        Counter {counterState.counter} +
+      </button>
+      <button onClick={() => decrementCounter(counterDispatch)}>
+        Counter {counterState.counter} -
+      </button>
       <h1>Olá Posts</h1>
       {postsState.loading && (
         <p>
